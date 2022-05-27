ea3232a8fc35c98e755df94548047a640e9bcd3d
Berat Can Dindar <beratcandindar24@hotmail.com>
2022-05-27 11:04:41 +0300

    playermesh dosyasında değiştirdik

--- Assets/Scripts/PlayerNavmesh.cs
+++ Assets/Scripts/PlayerNavmesh.cs
@@ -16,6 +16,7 @@ public class PlayerNavmesh : MonoBehaviour
     void Start()
     {
         navMeshAgent.destination = movePositionTransform.position;
+        
     }
     private void Update() {
         
