6c63dd18355d20ff050435059397c64eb0044553
Berat Can Dindar <beratcandindar24@hotmail.com>
2022-05-27 10:57:04 +0300

    looktarget scripti değişti

--- Assets/Scripts/LookatTarget.cs
+++ Assets/Scripts/LookatTarget.cs
@@ -10,7 +10,7 @@ public class LookatTarget : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("deneme");
     }
 
     // Update is called once per frame
